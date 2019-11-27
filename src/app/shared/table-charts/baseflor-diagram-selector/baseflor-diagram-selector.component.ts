9f1931f2175bf324c75eeb51bd3a08ac426689e0
Stephane <dev@phytoscopa.fr>
2019-11-27 16:34:21 +0100

    UI no table

--- src/app/shared/table-charts/baseflor-diagram-selector/baseflor-diagram-selector.component.ts
+++ src/app/shared/table-charts/baseflor-diagram-selector/baseflor-diagram-selector.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TableService } from 'src/app/_services/table.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'vl-baseflor-diagram-selector',
   templateUrl: './baseflor-diagram-selector.component.html',
   styleUrls: ['./baseflor-diagram-selector.component.scss']
 })
-export class BaseflorDiagramSelectorComponent implements OnInit {
+export class BaseflorDiagramSelectorComponent implements OnInit, OnDestroy {
   selecedValue: string = null;
   chartType: 'horizontalBar' | 'pie' = 'horizontalBar';
 
-  constructor() { }
+  tableSubscriber: Subscription;
+  tableSet = false;
+
+  constructor(private tableService: TableService) { }
 
   ngOnInit() {
+    this.tableSubscriber = this.tableService.currentTableChanged.subscribe(
+      value => {
+        if (value === true) {
+          this.tableSet = true;
+        } else {
+          this.tableSet = false;
+        }
+      }, error => { this.tableSet = false; }
+    );
+  }
+
+  ngOnDestroy() {
+
   }
 
   toggleChartType(): void {
