1ba9d4501268bd74adb44b56404edfde7344b3ab
Stéphane <stephanedelplanque@yahoo.fr>
2021-01-21 21:07:38 +0100

    Homepage design + Update colors

--- src/app/shared/home-page/home-page.component.ts
+++ src/app/shared/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { MenuService } from '../../_services/menu.service';
 import { OccurrenceService } from 'src/app/_services/occurrence.service';
 import { WorkspaceService } from 'src/app/_services/workspace.service';
+import { TableService } from 'src/app/_services/table.service';
 
 @Component({
   selector: 'vl-home-page',
@@ -11,20 +13,33 @@ import { WorkspaceService } from 'src/app/_services/workspace.service';
 })
 export class HomePageComponent implements OnInit {
   homeMenu = [];
+  selectedWorkflowItem = 1;
+
+  countReleves: number;
+  countRelevesMicrocenosis: number;
+  countRelevesSynusies: number;
+  countIdiotaxons: number;
+  countTables: number;
+  countTopLevelsReleves = true;
 
   constructor(private menuService: MenuService,
+              private tableService: TableService,
               private occurrenceService: OccurrenceService,
-              private wsService: WorkspaceService) { }
+              private wsService: WorkspaceService,
+              public router: Router) { }
 
   ngOnInit() {
     this.wsService.currentWS.next('none');
     this.menuService.setMenu(this.homeMenu);
-
-    /*const o = this.occurrenceService.getEsOccurrenceWithChildrenById(47);
-    o.subscribe(result => { console.log(result); });
-
-    const o2 = this.occurrenceService.getEsOccurrenceWithChildrenById(50);
-    o2.subscribe(result => { console.log(result); });*/
+    this.setCounters();
    }
+
+   setCounters(): void {
+    this.tableService.countTables(['phyto', 'none']).subscribe(result => this.countTables = result.count, error => { this.countTables = 0; console.log(error); });
+    this.occurrenceService.countReleves(['phyto', 'none'], [], this.countTopLevelsReleves).subscribe(result => this.countReleves = result.count, error => { this.countReleves = 0; console.log(error); });
+    this.occurrenceService.countReleves(['phyto', 'none'], ['microcenosis'], this.countTopLevelsReleves).subscribe(result => this.countRelevesMicrocenosis = result.count, error => { this.countRelevesMicrocenosis = 0; console.log(error); });
+    this.occurrenceService.countReleves(['phyto', 'none'], ['synusy'], this.countTopLevelsReleves).subscribe(result => this.countRelevesSynusies = result.count, error => { this.countRelevesSynusies = 0; console.log(error); });
+    this.occurrenceService.countIdiotaxons(['phyto', 'none']).subscribe(result => this.countIdiotaxons = result.count, error => { this.countIdiotaxons = 0; console.log(error); });
+  }
 
 }
